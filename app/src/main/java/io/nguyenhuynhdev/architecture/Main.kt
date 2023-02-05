8ef9d66dc4752b1bc20e66a1854c3f61888d5266
NguyenHuynh <nguyenhuynhdev@gmail.com>
2023-02-05 15:30:46 +0700

    Init

---
+++ app/src/main/java/io/nguyenhuynhdev/architecture/Main.kt
@@ -0,0 +1,11 @@
+package io.nguyenhuynhdev.architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
